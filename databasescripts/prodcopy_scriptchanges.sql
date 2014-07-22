7e99af9a8edc2bba80487d787265007e5d65dade
Herman Musiitwa <hmanmstw@gmail.com>
2014-07-22 19:46:29 +0300

    Merged conflicts in last commit

--- databasescripts/prodcopy_scriptchanges.sql
+++
@@ -1,5 +0,0 @@
--- Reset all email addresses to development addresses and the password to the word password
-UPDATE useraccount set email =  INSERT(email,LOCATE('@', email)+1, 22,'veritracker.com'), password = sha1('password');
-UPDATE company set email =  INSERT(email,LOCATE('@', email)+1, 22,'veritracker.com');
-
-UPDATE appconfig set optionvalue = 'admin@domain.com' WHERE optionname = 'emailmessagesender';
